973be068599150adac0c5b3f404e3cb707d6b6c7
Hussain158 <202105828@stu.uob.edu.bh>
2024-05-18 22:11:37 +0300

    Connecting to the server and asking user username.

--- client.py
+++ client.py
@@ -1,1 +1,8 @@
 import socket
+
+print("---------- Welcome to news API ----------")
+user_name = input("Please enter your username: ")
+print("Conecting to server ..")
+cs = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+cs.connect(("127.0.0.1",5349))
+print("---------- Server Connected Successfully ----------")
