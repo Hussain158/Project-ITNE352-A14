9f46a2fba685ec0a16d27be6c4379c7450c43811
Hussain158 <202105828@stu.uob.edu.bh>
2024-05-24 00:02:50 +0300

    added criteria's inputs and receive function

--- client.py
+++ client.py
@@ -2,7 +2,9 @@ import socket
 
 def send(option,cs):
     cs.send(option.encode("utf-8"))
-    
+
+def receive(cs):
+    cs.recv(2048)
 def main_menu(cs):
     print ("1.Search headlines\n")
     print ("2.List of scources\n")
@@ -10,12 +12,13 @@ def main_menu(cs):
     option = input("Choose an option = \n")
     if (option == 1):
         headline_search(cs)
-    elif (option == 2):
+    if (option == 2):
         list_of_sources(cs)
-    elif (option == 3):
+    if (option == 3):
         cs.close()
     else:
         print ("Invalid Option")
+        return
 
 def headline_search(cs):
     print("1.Search for keywords\n")
@@ -24,12 +27,33 @@ def headline_search(cs):
     print("4.List all new headlines\n")
     print("5.Back to main menu\n")
     option = input("Choose an option = \n")
-    if option in (1,2,3,4):
-        send(option,cs)
-    elif option == 5:
+    if option == 1:
+        criteria = input("Enter keyword: \n")
+        msg = f"1.{option}.{criteria}\n"
+        send(msg,cs)
+    if option == 2:
+        criteria = input("Enter category: (business, entertainment, general, health, science, sports, technology)\n")
+        msg = f"1.{option}.{criteria}\n"
+        if criteria != "business" and criteria!= "entertainment" and criteria!= "general" and criteria!= "health" and criteria!= "science" and criteria!= "sports" and criteria!= "technology":
+            print("Invalid Option")
+            return
+        msg = f"1.{option}.{criteria}\n"
+        send(msg,cs)
+    if option == 3:
+        criteria = input("Enter country code: (au, nz, ca, ae, sa, gb, us, eg, ma)\n")
+        msg = f"1.{option}.{criteria}\n"
+        if criteria != "au" and criteria!= "nz" and criteria!= "ca" and criteria!= "ae" and criteria!= "sa" and criteria!= "gb" and criteria!= "us" and criteria and criteria!= "eg" and criteria!= "ma":
+            print("Invalid Option")
+            return
+    if option == 4:
+        msg = f"1.{option}\n"
+        send(msg,cs)
+    if option == 5:
         main_menu(cs)
     else:
         print ("Invalid Option")
+        return
+    receive(cs)
         
 def list_of_sources(cs):
     print("1.Search by category\n")
@@ -40,10 +64,11 @@ def list_of_sources(cs):
     option = input("Choose an option = \n")
     if option in (1,2,3,4):
         send(option,cs)
-    elif option == 5:
+    if option == 5:
         main_menu(cs)
     else:
         print ("Invalid Option")
+        return
     
 def start_client():
     print("---------- Welcome to news API ----------\n")
