414c5b19d6549d32887baf78fef401328b31cd91
Hussain158 <202105828@stu.uob.edu.bh>
2024-05-23 22:21:13 +0300

    added headline function

--- client.py
+++ client.py
@@ -1,18 +1,33 @@
 import socket
 
-def main_menu():
-    print ("Search headlines\n")
-    print ("List of scources\n")
-    print ("Quit\n")
+def send(option,cs):
+    cs.send(option.encode("utf-8"))
+    
+def main_menu(cs):
+    print ("1.Search headlines\n")
+    print ("2.List of scources\n")
+    print ("3.Quit\n")
     option = input("Choose an option = \n")
     if (option == 1):
-        print ("\n")
+        headline_search()
     elif (option == 2):
         print ("\n")
-    elif (option == 3): 
-        print ("\n")
+    elif (option == 3):
+        cs.close()
+        
 
-
+def headline_search(cs):
+    print("1.Search for keywords\n")
+    print("2.Search by category\n")
+    print("3.Search by country\n")
+    print("4.List all new headlines\n")
+    print("5.Back to main menu\n")
+    option = input("Choose an option = \n")
+    if option in (1,2,3,4):
+        send(option,cs)
+    elif option == 5:
+        main_menu(cs)
+    
 def start_client():
     print("---------- Welcome to news API ----------\n")
     print("Conecting to server ..\n")
@@ -21,7 +36,7 @@ def start_client():
     print("----------Server Connected Successfully ----------\n")
     user_name = input("Enter your username: \n")
     cs.send(user_name.encode("utf-8"))
-    main_menu()
+    main_menu(cs)
 
 if __name__ == "__main__":
     start_client()
