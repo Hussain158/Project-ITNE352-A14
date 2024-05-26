09492395968b282c0b549adb41cd00f5b1b1b5b7
Alashawal <202105543@stu.uob.edu.bh>
2024-05-27 02:09:44 +0300

    major improvments and editingin server side, because it wasn't functional

--- server.py
+++ server.py
@@ -1,71 +1,81 @@
 import socket
 import threading
 import json
+import os
+
 from newsapi import NewsApiClient
 
-APIKEY = '844863c15fad42cba626fb66d2c24ef2'
-magazine_api = NewsApiClient(api_key=APIKEY)
+api = NewsApiClient(api_key='a86d8a517347451cb5335ce0b0f09bff')
 
-Server_host = "127.0.0.1"
-port = 5349
+def get_sources(request, lower_option, client_name):
+    #retrieve sources from API
+    if lower_option == '1':
+        data = api.get_sources(category=request)
+    elif lower_option == '2':
+        data = api.get_sources(country=request)
+    elif lower_option == '3':
+        data = api.get_sources(language=request)
+    elif lower_option == '4':
+        data = api.get_sources()
 
+    #Save only the first 15 sources in the json file
+    data["sources"] = data["sources"][0:15]
+    with open(os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), "database"),
+                            f'A14_{client_name}_2.{lower_option}.json'), 'w') as outfile:
+        json.dump(data, outfile)
 
-# start server
-def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((Server_host, port))
-    server_socket.listen(3)
-    print(f"Server is listening on {Server_host}:{port}")
+def get_headlines(request, lower_option, client_name):
+    #retrieve headlines from API
+    if lower_option == '1':
+        data = api.get_top_headlines(q=request)
+    elif lower_option == '2':
+        data = api.get_top_headlines(category=request)
+    elif lower_option == '3':
+        data = api.get_top_headlines(country=request)
+    elif lower_option == '4':
+        data = api.get_top_headlines()
+    data["articles"] = data["articles"][0:15]
+    with open(os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), "database"),
+                            f'A14_{client_name}_1.{lower_option}.json'), 'w') as outfile:
+        json.dump(data, outfile)
+def StartupOfServer():
+    ip_address = "127.0.0.1"
+    port_number = 4926
+    ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ss.bind((ip_address, port_number))
+    ss.listen(3)
+    print(f"Server is listening on...\n IP address: {ip_address} \n  Port number: {port_number}")
     while True:
-        cs, client_address = server_socket.accept()
-        client_handler = threading.Thread(target=client_handling, args=(cs, client_address))
+        cs, Client_IP = ss.accept()
+        client_handler = threading.Thread(target=client_handling, args=(cs, Client_IP))
         client_handler.start()
-
-
-# handeling the client request
 def client_handling(cs, address):
-    print(address, " is connected to the server")
+    print(f"Host {address} connected")
+    username = cs.recv(8192).decode()
+    print(f"Client {username} has connected to the server")
     while True:
-        username = cs.recv(2048).decode("utf-8")
-        user_message = cs.recv(2048).decode("utf-8")
-        if user_message == None:
+        try:
+            user_message = cs.recv(8192).decode()
+            if not user_message:
+                break
+            upper_option, lower_option,request = user_message.split(".")
+            if upper_option == "1":
+                get_headlines(request, lower_option, username)
+                data_extractionAndSending(upper_option, lower_option, username, cs)
+            elif upper_option == "2":
+                get_sources(request, lower_option, username)
+                data_extractionAndSending(upper_option, lower_option, username, cs)
+
+        except Exception as e:
+            print(f"Error handling client {username}: {e}")
             break
-        head_option, sub_option,request = user_message.split(".")
-        if head_option == '1':
-            headlines(request, sub_option, username)
-        elif head_option == '2':
-            sources(request, sub_option, username)
-
     cs.close()
-
-
-def headlines(request, option, client_name):
-    if option == '1':
-        data = magazine_api.get_top_headlines(q=request)
-    elif option == '2':
-        data = magazine_api.get_top_headlines(category=request)
-    elif option == '3':
-        data = magazine_api.get_top_headlines(country=request)
-    elif option == '4':
-        data = magazine_api.get_top_headlines()
-
-    with open(f'A14{client_name}1.{option}.json', 'w') as outfile:
-        json.dump(data, outfile)
-
-
-def sources(request, option, client_name):
-    if option == '1':
-        data = magazine_api.get_sources(category=request)
-    elif option == '2':
-        data = magazine_api.get_sources(country=request)
-    elif option == '3':
-        data = magazine_api.get_sources(language=request)
-    elif option == '4':
-        data = magazine_api.get_sources()
-
-    with open(f'A14{client_name}_2.{option}.json', 'w') as outfile:
-        json.dump(data, outfile)
-
-
+    print(f" {address} disconnected")
+def data_extractionAndSending(upper_option, lower_option, username, cs):
+    with open(os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), "database"),
+                           f'A14_{username}_{upper_option}.{lower_option}.json')) as f:
+        records = json.load(f)
+        cs.send(json.dumps(records).encode())
+        print(f"A14_{username}_{upper_option}.{lower_option} has been delivered to {username} successfully")
 if __name__ == "__main__":
-    start_server()
+    StartupOfServer()
